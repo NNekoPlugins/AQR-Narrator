1462fbcfdd946be9e92fd1c25f901dfd47db107a
NJR <nealjrobison@gmail.com>
2026-04-24 14:04:42 -0400

    - Initial commit and setup

---
+++ AQR-Narrator/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using Dalamud.Configuration;
+using Dalamud.Plugin;
+
+namespace NNekoPlugins.AqrNarrator;
+
+public class Configuration : IPluginConfiguration
+{
+    public int Version { get; set; } = 1;
+
+    public bool Enabled = true;
+
+    public void Save()
+        => AqrNarratorPlugin.PluginInterface.SavePluginConfig(this);
+}
